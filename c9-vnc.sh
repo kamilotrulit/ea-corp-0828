da61616d84244000d6d8310f82adf25c01d21af4
kamilotrulit <33978104+kamilotrulit@users.noreply.github.com>
2017-11-25 18:53:03 +0400

    Update and rename c9-vnc to c9-vnc.sh

---
+++ c9-vnc.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+sudo apt-get install libqt5x11extras5 -y
+
+sudo apt-get install thunar -y
+
+sudo apt-get install xarchiver -y
+
+cd ~
+
+git clone https://github.com/acabey/cloud9-vnc.git
+
+cd cloud9-vnc/
+
+sudo apt-get update
+
+sudo ./install.sh -y
+
+./run.sh
